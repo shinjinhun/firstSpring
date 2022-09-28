03e13b7956f59af8066da343b2b8d69b3887932f
shinjinhun <mamsorea@gmail.com>
2022-09-28 14:48:25 +0900

    업데이트

---
+++ src/main/java/com/firstSpring/app/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.firstSpring.app;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("HelloServlet init()");
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("HelloServlet service()");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("HelloServlet destroy()");
+    }
+}
